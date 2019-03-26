2da22f4ced156ad0e5a681ea6582854292198a79
Nishan <Nashrio907@gmail.com>
2019-03-27 01:02:24 +0600

    django form building

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,15 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .forms import ContactForm
+
 def home_page(request):
 	context = {
 	"title":"welcome to the home page"
-	}
-	return render(request, "home_page.html", context)
-
-def contact_page(request):
-	context = {
-	"title":"welcome to the contact page"
 	}
 	return render(request, "home_page.html", context)
 
@@ -18,3 +14,14 @@ def about_page(request):
 	"title":"welcome to the about page"
 	}
 	return render(request, "home_page.html", context)
+
+def contact_page(request):
+	contact_form = ContactForm(request.POST or None)
+	context = {
+	"title":"welcome to the about page",
+	"form" : contact_form
+	}
+	if contact_form.is_valid():
+		print(contact_form.cleaned_data)
+	print(request.POST)
+	return render(request, "contact/view.html", context)
