a6945fed67700a894cbf1a120fea6e0c4866adaa
Nishan <Nashrio907@gmail.com>
2019-04-02 21:34:40 +0600

    registrestion page added

--- ecommerce/forms.py
+++ ecommerce/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class ContactForm(forms.Form):
 
@@ -19,3 +22,28 @@ class ContactForm(forms.Form):
 class LoginForm(forms.Form):
 	username = forms.CharField()
 	password = forms.CharField(widget=forms.PasswordInput)
+
+
+
+class RegisterForm(forms.Form):
+	username = forms.CharField(widget=forms.TextInput(
+		attrs={"class":"form-control",}))
+	email    = forms.EmailField(widget=forms.EmailInput(
+		attrs={"class":"form-control",}))
+	password = forms.CharField(widget=forms.PasswordInput)
+	password2 = forms.CharField(label='Confirm password',widget=forms.PasswordInput)
+
+	def clean(self):
+		data = self.cleaned_data
+		password = self.cleaned_data.get('password')
+		password2 = self.cleaned_data.get('password2')
+		if password != password2:
+			raise forms.ValidationError("password must match")
+		return data
+
+	def clean_username(self):
+		username = self.cleaned_data.get('username')
+		qs = User.objects.filter(username=username)
+		if qs.exists():
+			raise forms.ValidationError("username is taken")
+		return username
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,13 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, get_user_model
 
-from .forms import ContactForm, LoginForm
+from .forms import ContactForm, LoginForm, RegisterForm
 
 def home_page(request):
 	context = {
 	"title":"welcome to the home page"
 	}
+	if request.user.is_authenticated:
+		context["premium_content"] = "yeahhh"
 	return render(request, "home_page.html", context)
 
 def about_page(request):
@@ -47,3 +49,20 @@ def login_page(request):
 			print("Error")
 
 	return render(request, "auth/login.html", context)
+
+
+User = get_user_model()
+def register_page(request):
+	form = RegisterForm(request.POST or None)
+	context = {
+	"form" : form
+	}
+	if form.is_valid():
+		print(form.cleaned_data)
+		username = form.cleaned_data.get("username")
+		email = form.cleaned_data.get("email")
+		password = form.cleaned_data.get("password")
+		new_user = User.objects.create_user(username, email, password)
+		print(new_user)
+    
+	return render(request, "auth/register.html", context)
