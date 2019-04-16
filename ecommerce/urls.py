98e31840c5effe4332d204321f91b45b59c21faf
Nishan <Nashrio907@gmail.com>
2019-04-16 19:49:14 +0600

    ListView of products added

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from .views import home_page, contact_page, about_page, login_page, register_page
+from products.views import ProductListView
 
 urlpatterns = [
 	url(r'^$', home_page),
@@ -26,6 +27,7 @@ urlpatterns = [
 	url(r'^register/', register_page),
 	url(r'^contact/', contact_page),
 	url(r'^about/', about_page),
+	url(r'^products/', ProductListView.as_view()),
     url(r'^admin/', admin.site.urls),
 ]
 if settings.DEBUG:
--- products/views.py
+++ products/views.py
@@ -1,3 +1,10 @@
+from django.views.generic import ListView
 from django.shortcuts import render
 
+from .models import Product
+
 # Create your views here.
+
+class ProductListView(ListView):
+	queryset = Product.objects.all()
+	template_name = "products/list.html"
