85e46943f30012659e4b7e2fbc592d12506817c5
Nishan <Nashrio907@gmail.com>
2019-04-16 20:38:38 +0600

    DetailView added

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
 
 from .models import Product
@@ -8,3 +8,17 @@ from .models import Product
 class ProductListView(ListView):
 	queryset = Product.objects.all()
 	template_name = "products/list.html"
+
+	# def get_context_data(self,*args,**kwargs): #for seeing what object are passing through the context(not necessary)
+	# 	context = super(ProductListView,self).get_context_data(*args, **kwargs)
+	# 	print(context)
+	# 	return context
+
+class ProductDetailView(DetailView):
+	queryset = Product.objects.all()
+	template_name = "products/detail.html"
+
+	# def get_context_data(self,*args,**kwargs): #for seeing what object are passing through the context(not necessary)
+	# 	context = super(ProductListView,self).get_context_data(*args, **kwargs)
+	# 	print(context)
+	# 	return context
