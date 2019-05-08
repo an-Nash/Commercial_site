38b4d2dbb754a172f45c5ef16092f70026bbc59f
Nishan <Nashrio907@gmail.com>
2019-05-08 23:56:43 +0600

    Template filter and forloop counter added

--- products/models.py
+++ products/models.py
@@ -52,6 +52,7 @@ class Product(models.Model):
 	price       = models.DecimalField(decimal_places=2,max_digits=20,default=39.99)
 	image       = models.ImageField(upload_to='products/', null=True, blank=True)
 	featured    = models.BooleanField(default=False)
+	timestamp	= models.DateTimeField(auto_now_add=True)
 
 
 	objects= ProductManager()
