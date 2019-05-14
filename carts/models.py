32815a52c03d19c0ad9ed04dddf23eaea8bb767a
Nishan <Nashrio907@gmail.com>
2019-05-14 21:26:09 +0600

    custom model manager part 2

--- carts/models.py
+++ carts/models.py
@@ -8,6 +8,27 @@ User = settings.AUTH_USER_MODEL
 # Create your models here.
 
 class CartManager(models.Manager):
+	def new_or_get(self, request):
+		cart_id = request.session.get("cart_id", None)
+		qs = Cart.objects.filter(id=cart_id)
+		if qs.count() == 1:
+			new_obj = False
+			print('Cart Id exists')
+			cart_obj = qs.first()
+			if request.user.is_authenticated() and cart_obj.user is None:
+				cart_obj.user = request.user
+				cart_obj.save()
+		else:
+			cart_obj = Cart.objects.new(user=request.user)
+			new_obj = True
+			request.session['cart_id'] = cart_obj.id
+			print("Cart Created")
+		return cart_obj, new_obj
+
+
+
+
+
 	def new(self, user=None):
 		user_obj = None
 		if user is not None:
