90e7a014a11fbae2e0acfa0f6ba6209c5284e723
Nishan <Nashrio907@gmail.com>
2019-06-16 16:52:41 +0600

    Join address with order

--- orders/models.py
+++ orders/models.py
@@ -4,6 +4,7 @@ from ecommerce.utils import unique_order_id_generator
 from django.db.models.signals import pre_save, post_save
 import math
 from billing.models import BillingProfile
+from addresses.models import Address
 
 
 # Create your models here.
@@ -33,15 +34,15 @@ class OrderManager(models.Manager):
 
 
 class Order(models.Model):
-	order_id 		= models.CharField(max_length=120, blank=True)
-	billing_profile = models.ForeignKey(BillingProfile, null=True, blank=True)
-	# shipping_addresses = 
-	# billing_addresses = 
-	cart 			= models.ForeignKey(Cart)
-	status			= models.CharField(max_length=120, default='created', choices=ORDER_STATUS_CHOICES)
-	shipping_total 	= models.DecimalField(default=5.99, max_digits=100, decimal_places=2)
-	total 			= models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
-	active			= models.BooleanField(default=True)
+	order_id 			= models.CharField(max_length=120, blank=True)
+	billing_profile 	= models.ForeignKey(BillingProfile, null=True, blank=True)
+	shipping_address 	= models.ForeignKey(Address, related_name="shipping_address", null=True, blank=True)
+	billing_address 	= models.ForeignKey(Address, related_name="billing_address", null=True, blank=True)
+	cart 				= models.ForeignKey(Cart)
+	status				= models.CharField(max_length=120, default='created', choices=ORDER_STATUS_CHOICES)
+	shipping_total 		= models.DecimalField(default=5.99, max_digits=100, decimal_places=2)
+	total 				= models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
+	active				= models.BooleanField(default=True)
 
 	def __str__(self):
 		return self.order_id
