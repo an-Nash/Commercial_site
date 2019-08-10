ff83a89fd8aa8b78bcde35f881af7d264c5e7e94
Nishan <Nashrio907@gmail.com>
2019-08-11 01:02:48 +0600

    forms and admin for custom user

--- accounts/models.py
+++ accounts/models.py
@@ -60,6 +60,12 @@ class User(AbstractBaseUser):
 	def get_short_name(self):
 		return self.email
 
+	def has_perm(self, prem, obj=None):
+		return True
+
+	def has_module_perms(self, app_label):
+		return True
+
 	@property
 	def is_staff(self):
 		return self.staff
