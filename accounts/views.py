474c02fa9f330c28db8ca6e22a9213a23bf1ff16
Nishan <Nashrio907@gmail.com>
2019-08-27 19:57:25 +0600

    Handling and ending user session

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, get_user_model
 from django.utils.http import is_safe_url
+from .signals import user_logged_in
 
 from .forms import LoginForm, RegisterForm, GuestForm
 from .models import GuestEmail
@@ -73,6 +74,7 @@ class LoginView(FormView):
 		user = authenticate(request, email=email, password=password)
 		if user is not None:
 			login(request, user)
+			user_logged_in.send(user.__class__, instance=user, request=request)
 			try:
 				del request.session['guest_email_id']
 			except:
--- analytics/models.py
+++ analytics/models.py
@@ -2,11 +2,15 @@ from django.db import models
 from django.conf import settings
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.db.models.signals import pre_save, post_save
+from django.contrib.sessions.models import Session
 
-
+from accounts.signals import user_logged_in
 from .signals import object_viewed_signal
 from .utils import get_client_ip
 # Create your models here.
+
+
 User = settings.AUTH_USER_MODEL
 
 class ObjectViewed(models.Model):
@@ -27,10 +31,6 @@ class ObjectViewed(models.Model):
 
 def object_viewed_receiver(sender, instance, request, *args, **kwargs):
 	c_type = ContentType.objects.get_for_model(sender)
-	# print(sender)
-	# print(instance)
-	# print(request)
-	# print(request.user)
 
 	new_view_obj = ObjectViewed.objects.create(
 		user = request.user,
@@ -41,3 +41,49 @@ def object_viewed_receiver(sender, instance, request, *args, **kwargs):
 		)
 
 object_viewed_signal.connect(object_viewed_receiver)
+
+class UserSession(models.Model):
+	user 			= models.ForeignKey(User, blank=True, null=True)
+	ip_address		= models.CharField(max_length=220, blank=True, null=True)
+	session_key 	= models.CharField(max_length=100, null=True, blank=True)
+	timestamp		= models.DateTimeField(auto_now_add=True)
+	active 			= models.BooleanField(default = True)
+	ended			= models.BooleanField(default = False)
+
+	def end_session(self):
+		session_key = self.session_key
+		ended = self.ended
+		try:
+			Session.objects.get(pk = session_key).delete()
+			self.active = False
+			self.ended = True
+			self.save()
+		except:
+			pass
+		return self.ended
+
+
+
+def post_save_session_receiver(sender, instance, created, *args, **kwargs):
+	if created:
+		qs = UserSession.objects.filter(user=instance.user, ended=False, active=False).exclude(id=instance.id)
+		for i in qs:
+			i.end_session()
+
+
+post_save.connect(post_save_session_receiver, sender=UserSession)
+
+
+def user_logged_in_receiver(sender, instance, request, *args, **kwargs):
+	print(instance)
+	user = instance
+	ip_address = get_client_ip(request)
+	session_key = request.session.session_key
+	UserSession.objects.create(
+		user = user,
+		ip_address = ip_address,
+		session_key = session_key
+		)
+
+
+user_logged_in.connect(user_logged_in_receiver)
